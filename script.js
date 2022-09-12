4c7d8e24ce557a7e7cecfa7febe71c6420d1c61c
Adams Fontil <adamsfuntil@gmail.com>
2022-09-12 10:12:03 -0400

    Add grid, and make hovering work so that it blacks out each square. Also have different buttons to webpage that'll soon do things. And finally use css to line things up on site

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-alert('It works')
+
+// creating divs is done
+const container = document.querySelector(".container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+    hover()
+  };
+};
+// rows = prompt('Size: ')
+rows = 16
+makeRows(rows, rows);
+let color;
+
+function rainbw() {  // this function will let the computer pick at random rock, paper or scissors.//
+    randomNum = Math.floor(Math.random() * 5); // this is done by first picking a random number from 0 to 2//
+    if (randomNum === 0) { //after the number is chosen, the results will be assigned a choice//
+        color = "red"
+    } else if (randomNum === 1) {
+        color = "blue"
+    } else if (randomNum === 2) {
+        color = "yellow"
+    }
+    return color // this what this function ultimately returns, one choice rock, paper, or scissors.
+
+}
+
+
+
+function hover() {
+
+container.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'black';
+});
+}
